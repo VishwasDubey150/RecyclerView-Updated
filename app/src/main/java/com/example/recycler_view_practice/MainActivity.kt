070494ea4676f671c83a5282a45813d8439792eb
Vishwas <VishwasDubey150@gmail.com>
2023-02-18 22:14:24 +0530

    searching

--- app/src/main/java/com/example/recycler_view_practice/MainActivity.kt
+++ app/src/main/java/com/example/recycler_view_practice/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.recycler_view_practice
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,19 +17,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var newAL: ArrayList<list>
     private lateinit var image: Array<Int>
     private lateinit var name: Array<String>
+    private lateinit var temparrayList:ArrayList<list>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
         image = arrayOf(
             R.drawable.vk,
             R.drawable.srk,
             R.drawable.em,
             R.drawable.zk,
             R.drawable.sm,
-            R.drawable.ab)
+            R.drawable.ab,
+            R.drawable.lm,
+            R.drawable.cr,
+            R.drawable.ms,
+            R.drawable.mz,
+            R.drawable.rt)
 
         name = arrayOf(
             "Virat Kohli",
@@ -35,14 +42,59 @@ class MainActivity : AppCompatActivity() {
             "Elon Musk",
             "Zakir Khan",
             "Shawn Mendes",
-            "Abhishek Bisht")
+            "Abhishek Bisht",
+            "lionel Messi",
+            "Cristiano Ronaldo",
+            "Ms Dhoni",
+            "Mark Zuckerberg",
+            "Ratan Tata")
 
         var rv = findViewById<RecyclerView>(R.id.rv)
         rv.layoutManager = LinearLayoutManager(this)
         rv.setHasFixedSize(true)
 
         newAL = arrayListOf<list>()
+        temparrayList= arrayListOf<list>()
         getUserdata()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var rv=findViewById<RecyclerView>(R.id.rv)
+        menuInflater.inflate(R.menu.menu,menu)
+        val item=menu?.findItem(R.id.search_action)
+        val searchView=item?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                temparrayList.clear()
+                val searchText=newText!!.toLowerCase(Locale.getDefault())
+                if(searchText.isNotEmpty())
+                {
+                    newAL.forEach {
+                        if(it.name.toLowerCase(Locale.getDefault()).contains(searchText))
+                        {
+                            temparrayList.add(it)
+                        }
+                    }
+                    rv.adapter!!.notifyDataSetChanged()
+                }
+                else
+                {
+                    temparrayList.clear()
+                    temparrayList.addAll(newAL)
+                    rv.adapter!!.notifyDataSetChanged()
+                }
+
+                return false
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun getUserdata() {
@@ -50,10 +102,12 @@ class MainActivity : AppCompatActivity() {
             val Lists = list(image[i], name[i])
             newAL.add(Lists)
         }
+
+        temparrayList.addAll(newAL)
         var rv = findViewById<RecyclerView>(R.id.rv)
         rv.layoutManager = LinearLayoutManager(this)
 
-        var adapter = MyAdapter(newAL)
+        var adapter = MyAdapter(temparrayList)
         rv.adapter = adapter
 
 
@@ -92,8 +146,6 @@ class MainActivity : AppCompatActivity() {
         val touchHelper=ItemTouchHelper(swipeGesture)
         touchHelper.attachToRecyclerView(rv)
 
-
-
         adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener {
             override fun onItemClick(position: Int) {
                 Toast.makeText(this@MainActivity,
@@ -101,9 +153,5 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT).show()
             }
         })
-
-
-
-
     }
 }
